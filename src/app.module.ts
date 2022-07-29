bb23c1670d3711d075c4b08fd8f06cae5e82996b
K B Tanvir <tanvirkhaan004@gmail.com>
2022-07-29 10:40:40 +0600

    local strategy testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASS}@cluster0.wpl9a.gcp.mongodb.net/?retryWrites=true&w=majority`,
-    ),
-  ],
   controllers: [AppController],
   providers: [AppService],
+  imports: [AuthModule, UsersModule],
 })
 export class AppModule {}
