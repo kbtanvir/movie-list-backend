0423c92d56debcf9d6842c0c448464da818f6703
kbtanvir <tanvirkhaan004@gmail.com>
2023-02-06 11:42:59 +0600

    movie list debugging done

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,43 +6,46 @@ import {
   HttpCode,
   Param,
   Patch,
-  Post
+  Post,
 } from '@nestjs/common';
 import { MoviesEntity } from './entity/movie-entity';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
 // @UseGuards(JwtAuthGuard)
- export class MoviesController {
-   constructor(private moviesService: MoviesService) {}
+export class MoviesController {
+  constructor(private moviesService: MoviesService) {}
 
-   @HttpCode(200)
-   @Get()
-   async getMoviesList() {
-     return this.moviesService.getAllItems();
-   }
+  @HttpCode(200)
+  @Get()
+  async getMoviesList() {
+    return this.moviesService.getAllItems();
+  }
 
-   @HttpCode(200)
-   @Get(':id')
-   async readItem(@Param('id') id: string) {
-     return this.moviesService.readItem(id);
-   }
+  @HttpCode(200)
+  @Get(':id')
+  async readItem(@Param('id') id: string) {
+    return await this.moviesService.readItem(id);
+  }
 
-   @HttpCode(200)
-   @Post()
-   async createItem(@Body() dto: MoviesEntity) {
-     return this.moviesService.createItem(dto);
-   }
+  @HttpCode(200)
+  @Post()
+  async createItem(@Body() dto: MoviesEntity) {
+    return await this.moviesService.createItem(dto);
+  }
 
-   @HttpCode(200)
-   @Patch(':id')
-   async updateItem(@Param('id') id: string, @Body() dto: Partial<MoviesEntity>) {
-     return this.moviesService.updateItem(id, dto);
-   }
+  @HttpCode(200)
+  @Patch(':id')
+  async updateItem(
+    @Param('id') id: string,
+    @Body() dto: Partial<MoviesEntity>,
+  ) {
+    return await this.moviesService.updateItem(id, dto);
+  }
 
-   @HttpCode(200)
-   @Delete(':id')
-   async deleteItem(@Param('id') id: string) {
-     return this.moviesService.deleteItem(id);
-   }
- }
+  @HttpCode(200)
+  @Delete(':id')
+  async deleteItem(@Param('id') id: string) {
+    return await this.moviesService.deleteItem(id);
+  }
+}
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { ReturnModelType } from '@typegoose/typegoose';
 
@@ -14,24 +14,35 @@ export class MoviesService {
   ) {}
 
   async getAllItems(): Promise<MoviesEntity[]> {
-    return this.model.find().exec();
+    return await this.model.find().exec();
   }
 
   async readItem(id: string): Promise<MoviesEntity> {
-    return this.model.findOne({ _id: id }).exec();
+    return await this.model.findOne({ _id: id }).exec();
   }
   async createItem(dto: MoviesEntity): Promise<MoviesEntity> {
     const createdCat = new this.model(dto);
-    return createdCat.save();
+    return await createdCat.save();
   }
   async updateItem(
     id: string,
     dto: Partial<MoviesEntity>,
   ): Promise<MoviesEntity> {
-    return this.model.findOneAndUpdate({ _id: id }, dto, { new: true });
+    const res = await this.model.findOneAndUpdate({ _id: id }, dto, {
+      new: true,
+    });
+    if (!res) throw new NotFoundException('Item not found');
+    return res;
   }
 
-  async deleteItem(id: string): Promise<void> {
-    this.model.findOneAndDelete({ _id: id }).exec();
+  async deleteItem(id: string): Promise<any> {
+    const res = await this.model.findOneAndDelete({ _id: id });
+
+    if (!res) throw new NotFoundException('Item not found');
+
+    return {
+      deleted: true,
+      id: res._id,
+    };
   }
 }
